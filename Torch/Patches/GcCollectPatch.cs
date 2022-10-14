90ff3f93f09a4e96c6a33a67e0f1f9b9a3159a49
zznty <94796179+zznty@users.noreply.github.com>
2022-10-15 00:44:49 +0700

    disable gc patch because conflicting with the same from plugin

--- Torch/Patches/GcCollectPatch.cs
+++ Torch/Patches/GcCollectPatch.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if false
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
@@ -51,3 +52,4 @@ internal static class GcCollectPatch
         }
     }
 }
+#endif
